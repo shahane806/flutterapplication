ad8fc2db81bd2088f76c9b07f1201d7c5fb9a14b
Om Shahane <71377782+shahane806@users.noreply.github.com>
2025-03-26 17:27:06 +0530

    Delete lib directory

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'MultipleBlocProviderWrapper/MultipleBlocProviderWrapper.dart';
-import 'MyApp/MyApp.dart';
-
-void main() {
-  runApp(MultipleBlocProviderWrapper(const MyApp()));
-}
