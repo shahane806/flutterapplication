0b045d1bb8f826b03b36b0bbe94457f38410cfe2
Om Shahane <71377782+shahane806@users.noreply.github.com>
2025-04-02 15:52:57 +0530

    Delete lib directory

--- lib/StateManagement/UserDashboardStateManagement/Blocs/UserDashboardBloc.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../Events/UserDashboardEvent.dart';
-import '../States/UserDashboardState.dart';
-
-class UserDashboardBloc extends Bloc<UserDashboardEvent, UserDashboardState> {
-  UserDashboardBloc() : super(UserDashboardState.initial()) {}
-}
