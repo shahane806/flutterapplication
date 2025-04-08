c8b905418ff361899df2be1a2ffcc8ed7430c5ff
Om Shahane <71377782+shahane806@users.noreply.github.com>
2025-04-08 22:29:44 +0530

    Delete lib directory

--- lib/Handlers/AuthStatus.dart
+++
@@ -1,1 +0,0 @@
-enum AuthStatus { loginFailed, loggedIn, loggedOut }
--- lib/Handlers/BaseUrl.dart
+++
@@ -1,4 +0,0 @@
-class Apis {
-  static final String BaseUrl = 'http://192.168.1.52:9003/';
-// static final String BaseUrl = 'https://ce7a-115-244-240-2.ngrok-free.app/';
-}
