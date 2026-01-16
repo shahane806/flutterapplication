50fb85ba478d7721856e2e98578abebaf62ae888
Om Shahane <71377782+shahane806@users.noreply.github.com>
2026-01-16 10:13:11 +0530

    Fix import paths in main.dart
    Updated import paths for MultipleBlocProviderWrapper and MyApp.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'MultipleBlocProviderWrapper/MultipleBlocProviderWrapper.dart';
-import 'MyApp/MyApp.dart';
+import 'MultipleBlocProviderWrapper/MultipleBlocProviderWrapper.dart';    //change this to : multiple_bloc_provider_wrapper/multiple_bloc_provider_wrapper.dart
+import 'MyApp/MyApp.dart'; //my_app/my_app.dart
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MultipleBlocProviderWrapper(const MyApp()));
+  runApp(
+    MultipleBlocProviderWrapper(
+      const MyApp()
+    )
+  );
 }
+
