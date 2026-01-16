b0e4c96e622aeb0838317798849c41b862ced951
Om Shahane <71377782+shahane806@users.noreply.github.com>
2026-01-16 10:22:31 +0530

    Refactor navigation logic in SplashScreen
    Refactor SplashScreen to use a separate method for navigation after a delay and improve code readability.

--- lib/Screens/SplashScreen/SplashScreen.dart
+++ lib/Screens/SplashScreen/SplashScreen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -16,32 +14,53 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 5), () async {
-      SharedPreferences pref = await SharedPreferences.getInstance();
-      String mobile = pref.get("mobile").toString();
+    _navigateAfterDelay();
+  }
 
-      if (mobile != '') {
+  /// Navigates after a delay of [seconds] to the appropriate screen
+  Future<void> _navigateAfterDelay({int seconds = 5}) async {
+    await Future.delayed(Duration(seconds: seconds));
+
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? mobile = prefs.getString('mobile');
+
+    if (mobile != null && mobile.isNotEmpty) {
+      // Navigate to LoginScreen if mobile exists
+      if (mounted) {
         Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const LoginScreen(),
-            )); // Navigate to the next screen
+          context,
+          MaterialPageRoute(builder: (_) => const LoginScreen()),
+        );
       }
-    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-            child: ClipOval(
-          child: Image.asset(
-            'assets/images/logo.png',
-            width: MediaQuery.of(context).size.width * 0.55,
-            height: MediaQuery.of(context).size.height * 0.25,
-            fit: BoxFit.cover, // Adjusts how the image is displayed
-          ),
-        )));
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipOval(
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: screenWidth * 0.55,
+                height: screenHeight * 0.25,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
